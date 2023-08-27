400a9e8b5fd7dcc45feee622aafb498b3c96df6d
RuikoTsutsumi <128773768+RuikoTsutsumi@users.noreply.github.com>
2023-08-27 11:05:55 +0900

    AppserviceProvider修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +23,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        if(\App::environment(['production'])|| (\App::environment(['develop'])){
-            \URL::forceScheme('https');
-        }
+        // if(\App::environment(['production'])|| (\App::environment(['develop'])){
+        //     URL::forceScheme('https');
+        // }
+        if (App::environment(['production']) || App::environment(['develop'])) {
+            URL::forceScheme('https');
+        };
     }
 }
